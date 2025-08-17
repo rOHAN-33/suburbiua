771bf44bf670b4e81281c4c993dc756f8ae19b84
rohannegi484@gmail.com <rohannegi484@gmail.com>
2025-08-18 01:50:45 +0530

    first commit

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,57 @@
+import { FC } from "react";
+import { Content } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { PrismicNextLink } from "@prismicio/next";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero: FC<HeroProps> = ({ slice }) => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      
+      {/**
+       * 💡 Use Prismic MCP with your code editor
+       *
+       * Get AI-powered help to build your slice components — based on your actual model.
+       *
+       * ▶️ Setup:
+       * 1. Add a new MCP Server in your code editor:
+       *
+       * {
+       *   "mcpServers": {
+       *     "Prismic MCP": {
+       *       "command": "npx",
+       *       "args": ["-y", "@prismicio/mcp-server@latest"]
+       *     }
+       *   }
+       * }
+       *
+       * 2. Select a model optimized for coding (e.g. Claude 3.7 Sonnet or similar)
+       *
+       * ✅ Then open your slice file and ask your code editor:
+       *    "Code this slice"
+       *
+       * Your code editor reads your slice model and helps you code faster ⚡
+       * 🎙️ Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
+       * 📚 Documentation: https://prismic.io/docs/ai#code-with-prismics-mcp-server
+       */}
+       <div className="text-red-600">
+<PrismicRichText field={slice.primary.heading} />
+       </div>
+       
+       <PrismicRichText field={slice.primary.body} />
+       <PrismicNextLink field={slice.primary.button} />
+    </section>
+  );
+};
+
+export default Hero;
